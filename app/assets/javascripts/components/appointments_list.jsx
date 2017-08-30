d7f6a08f78207fdff67be40d73067b6faf4d865c
Wendersonandes <oldmaverick@gmail.com>
2017-08-30 16:49:06 -0300

    Added appointments_list component

---
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -0,0 +1,14 @@
+var AppointmentsList = React.createClass({
+	render: function(){
+		return(
+			<div>
+				{this.props.appointments.map( function(appointment){
+					return(	
+						< Appointment appointment = {appointment} key={appointment.id}/>
+					)
+				})}
+
+			</div>
+		)
+		}
+})
