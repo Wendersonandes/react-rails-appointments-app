7abd7c78fb1026c7ca60aba96e4c84685013a776
Wendersonandes <oldmaverick@gmail.com>
2017-08-31 15:23:36 -0300

    Added update method to controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,7 @@
 class AppointmentsController < ApplicationController
+
+	before_action :set_appointment, :only => [:update]
+
   def index
 		@appointments = Appointment.order('apt_time ASC')
 		@appointment = Appointment.new
@@ -13,7 +16,19 @@ class AppointmentsController < ApplicationController
 		end
 	end
 
+	def update
+		if @appointment.update(appointment_params)
+			render :json => @appointment
+		else
+			render :json => @appointment.errors, :status => :unprocessable_entity
+		end
+	end
+
 	private
+
+	def set_appointment
+		@appointment = Appointment.find(params[:id])
+	end
 
 	def appointment_params
 		params.require(:appointment).permit(:id, :title, :apt_time)
