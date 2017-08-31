0f6455f89b2e83a543cc56717cb943ecdf066c2a
Wendersonandes <oldmaverick@gmail.com>
2017-08-31 18:03:06 -0300

    Passing updateAppointment prop to appointment component

--- app/assets/javascripts/components/appointments_list.jsx
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -1,10 +1,12 @@
 var AppointmentsList = React.createClass({
 	render: function(){
+		updateAppointment = this.props.updateAppointment;
+
 		return(
 			<div>
 				{this.props.appointments.map( function(appointment){
 					return(	
-						< Appointment appointment = {appointment} key={appointment.id}/>
+						< Appointment appointment = {appointment} key={appointment.id} updateAppointment={updateAppointment}/>
 					)
 				})}
 
