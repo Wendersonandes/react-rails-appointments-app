541166d409be0bec85f78adba81512884ee1309d
Wendersonandes <oldmaverick@gmail.com>
2017-08-31 15:22:48 -0300

    Added default csrf token config to axios header

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,3 +1,11 @@
 var formatDate = function(d){
 	return	moment(d).format('MMMM DD YYYY, h:mm:ss a');
 }
+
+function setAxiosHeaders(){
+	var token = document.getElementsByName('csrf-token');
+	axios.defaults.headers.common['X-CSRF-Token'] = token[0].getAttribute('content');
+	axios.defaults.headers.common['Accept'] = 'application/json';
+};
+
+setAxiosHeaders();
