50e40e2a0a03910fd038736b1e21b30cd5c4a5bb
Wendersonandes <oldmaverick@gmail.com>
2017-09-01 02:31:08 -0300

    Added destroy method / index method rendering json response

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,10 +1,14 @@
 class AppointmentsController < ApplicationController
 
-	before_action :set_appointment, :only => [:update]
+	before_action :set_appointment, :only => [:update, :destroy]
 
   def index
 		@appointments = Appointment.order('apt_time ASC')
 		@appointment = Appointment.new
+		respond_to do |format|
+			format.html
+			format.json { render :json => @appointments }
+		end
   end
 
 	def create
@@ -22,6 +26,11 @@ class AppointmentsController < ApplicationController
 		else
 			render :json => @appointment.errors, :status => :unprocessable_entity
 		end
+	end
+
+	def destroy
+		@appointment.destroy
+		render :nothing => true, :status => 204
 	end
 
 	private
