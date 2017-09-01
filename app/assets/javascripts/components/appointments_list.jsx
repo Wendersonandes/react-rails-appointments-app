a7fb53ce35b2cc71df34ad7e82de34967e31261f
Wendersonandes <oldmaverick@gmail.com>
2017-09-01 02:33:57 -0300

    Passing onDelete as props

--- app/assets/javascripts/components/appointments_list.jsx
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -1,16 +1,16 @@
 var AppointmentsList = React.createClass({
 	render: function(){
 		updateAppointment = this.props.updateAppointment;
+		onDelete = this.props.onDelete;
 
 		return(
 			<div>
 				{this.props.appointments.map( function(appointment){
 					return(	
-						< Appointment appointment = {appointment} key={appointment.id} updateAppointment={updateAppointment}/>
+						< Appointment appointment = {appointment} key={appointment.id} updateAppointment={updateAppointment} onDelete={onDelete} />
 					)
 				})}
-
 			</div>
 		)
-		}
+	}
 })
